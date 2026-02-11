decaef90704ab7219bf20a8774c96a51c2223993
Allen <117472046+allencheng0728@users.noreply.github.com>
2026-02-11 11:20:05 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
         base: '/feature-illustration2/',
+      server: {
         port: 3000,
         host: '0.0.0.0',
       },
@@ -21,4 +21,5 @@ export default defineConfig(({ mode }) => {
         }
       }
     };
+
 });
